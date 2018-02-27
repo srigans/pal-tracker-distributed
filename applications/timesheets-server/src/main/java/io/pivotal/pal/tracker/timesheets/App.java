0bc72aede5da061ecbde298c18dc5ae973cce2b1
Stubhub PAL <stubhubpal@gmail.com>
2018-02-27 10:37:01 -0800

    Revert "Get circuit-breaker setup locally"
    This reverts commit 1b7af3a07a0a4d3620a1e64c6f0aae1ba2a7f938.

--- applications/timesheets-server/src/main/java/io/pivotal/pal/tracker/timesheets/App.java
+++ applications/timesheets-server/src/main/java/io/pivotal/pal/tracker/timesheets/App.java
@@ -3,7 +3,6 @@ package io.pivotal.pal.tracker.timesheets;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -15,7 +14,6 @@ import java.util.TimeZone;
 @SpringBootApplication
 @ComponentScan({"io.pivotal.pal.tracker.timesheets", "io.pivotal.pal.tracker.restsupport"})
 @EnableEurekaClient
-@EnableCircuitBreaker
 public class App {
 
     public static void main(String[] args) {
